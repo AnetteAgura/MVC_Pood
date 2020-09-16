b479b9b5e7d3a9da11ff2175b657421d3363138a
Anette <anetteagura@gmail.com>
2020-09-16 11:17:40 +0300

    Created a shop and products to shop.

--- ui.py
+++ ui.py
@@ -2,13 +2,17 @@
 
 # import classes and files
 from product import Product
+from shop import Shop
 
 # create products
 bread = Product("bread", 0.80, 10)
-milk = Product("bread", 0.50, 50)
-wine = Product("bread", 5.60, 5)
+milk = Product("milk", 0.50, 50)
+wine = Product("wine", 5.60, 5)
 
-# test product view
-print(bread)
-print(milk)
-print(wine)
+# create shop and add products to shop
+shop = Shop()
+shop.addProduct(bread)
+shop.addProduct(milk)
+shop.addProduct(wine)
+
+print(shop)
