bec3b715dafc9461c8d829af83f0d2eeacb90ad1
Anette <anetteagura@gmail.com>
2020-09-16 11:46:36 +0300

    Added punch of files, but right now can't see what's in the shop.

---
+++ exceptions.py
@@ -0,0 +1,6 @@
+class ItemExists(Exception):
+    pass
+
+
+class ItemNotExists(Exception):
+    pass
---
+++ helpers.py
@@ -0,0 +1,17 @@
+import exceptions
+from product import Product
+
+# represents shop structure
+# list of Product type objects
+items = []
+
+# add item to items
+def addItem(name, price, amount):
+    global items
+    # create product with required description
+    product = Product(name, price, amount)
+    # check if item already exists
+    if product in items:
+        raise exceptions.ItemExists("Item {} exists.".format(name))
+    else:
+        items.append(product)
